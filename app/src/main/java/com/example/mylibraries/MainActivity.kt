efb495601cbada25ba205ea918ca2770df46fefc
Umang Choudhary <umang.choudhary1410@gmail.com>
2022-02-26 16:31:57 +0530

    add library in same app locally

--- app/src/main/java/com/example/mylibraries/MainActivity.kt
+++ app/src/main/java/com/example/mylibraries/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.mylibraries
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.emailvalidator.EmailValidator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = "umang.choudhary1410@gmail.com"
+
+        val emailValidator = EmailValidator()
+        val result = emailValidator.isValidEmail(email)
     }
 }
